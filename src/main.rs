48aa7d50da1dca8807db7b2fa97fc3c269335718
Simon Marache <github@simon.marache.net>
2023-08-20 17:38:37 +0000

    Set default logging to INFO

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,9 @@ use crate::decrypt::{
 use crate::encrypt::encode_xor;
 
 fn main() {
-    env_logger::init();
+    env_logger::init_from_env(
+        env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
+    );
 
     set1();
 
